580e285d030543ebaea073075d6e8ab8871f7102
kaminaritukane <kaminaritukane@163.com>
2020-03-16 15:32:02 +0800

    Sort usings

--- UnitySVON/Assets/Scripts/SVONWrapper.cs
+++ UnitySVON/Assets/Scripts/SVONWrapper.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Win32.SafeHandles;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
--- UnitySVON/Assets/Scripts/TestCode.cs
+++ UnitySVON/Assets/Scripts/TestCode.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Unity.Collections;
-using Unity.Jobs;
 using UnityEngine;
 
 public class TestCode : MonoBehaviour
